188b3bcff2baa804e156d2f5efebd3a4ea3dd0d2
Muhammad Acla <muhamadacla22@gmail.com>
2025-02-26 23:27:52 +0700

    init

---
+++ android/src/main/java/twilight/cat/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package twilight.cat.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import twilight.cat.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
